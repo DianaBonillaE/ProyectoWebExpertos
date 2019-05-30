af41792d1c27b1e9595010d20771aa6c379e92eb
Esteban Barrantes Mora <39637041+EstebanBarrantesMora95@users.noreply.github.com>
2019-05-30 16:10:06 -0600

    Tipo de turismo y Servicio
    CRUD tipo de turismo
    CRUD servicio

---
+++ proyectoWeb/views/DeleteTypeTurism.php
@@ -0,0 +1,45 @@
+<?php include 'header.php';?>
+<div class="container md-3">
+	<br />
+        <h1 class="text-center">Esta seguro que desea Eliminar </h1>
+        <br />
+ 	<div class="panel-body">
+		<div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-8">
+                <form class="form-inline">
+		<div class="row">
+                	<div class="col-md-3"></div>
+                	<div class="col-md-8">
+                		<form class="form-inline">
+				<div class="form-group">
+                         	<h4>Nombre</h4>
+                </div>
+			               Rural
+					</div>
+					
+                		</form>
+                </div>
+	</div>
+	</div>
+	</div>
+	<br>
+	
+	<div class="row justify-content-center">
+                        	<div class="col-md-1">
+                        	</div>
+                        	<div class="form-group">
+                            		<div class="col-md-2">
+                               		<a href="./indexTypeTurism.php" class="btn btn-primary btn-md text-uppercase">Cancelar</a>
+                           		</div>
+                        	</div>
+
+                        <div class="form-group">
+                            <div class="col-md-2">
+                                <input type="submit" value=Eliminar class="btn btn-primary btn-md text-uppercase" />
+                            </div>
+	
+			</div>
+</div>
+
+
---
+++ proyectoWeb/views/EditService.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+<div class="container md-3">
+	<br />
+        <h1 class="text-center">Modificar Servicio</h1>
+        <br />
+ 	<div class="panel-body">
+		<div class="row">
+                <div class="col-md-3"></div>
+                <div class="col-md-8">
+                <form class="form-inline">
+		<div class="row">
+                	<div class="col-md-3"></div>
+                	<div class="col-md-8">
+                		<form class="form-inline">
+				<div class="form-group">
+                         	<h4>Nombre</h4>
+                        		<div class="col-md-10">
+                        		<input type="text" name="name" id="name" />
+                        		</div>
+								<h4>Descripción</h4>
+                        		<div class="col-md-10">
+                        		<input  type="text" name="descripcion" id="descripcion" />
+                        		</div>
+                </div>
+			
+					</div>
+                		</form>
+                </div>
+	</div>
+	</div>
+	</div>
+	<br>
+	
+	<div class="row justify-content-center">
+                        	<div class="col-md-1">
+                        	</div>
+                        	<div class="form-group">
+                            		<div class="col-md-2">
+                               		<a href="./indexService.php" class="btn btn-primary btn-md text-uppercase">Cancelar</a>
+                           		</div>
+                        	</div>
+
+                        <div class="form-group">
+                            <div class="col-md-2">
+                                <input type="submit" value=Editar class="btn btn-primary btn-md text-uppercase" />
+                            </div>
+	
+			</div>
+</div>
+
+
